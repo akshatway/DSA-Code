4c4a9f5fb8b705dcd579c3671c209f62b0c43a55
Akshat <akshat@gmail.com>
2026-04-19 14:25:13 +0530

    updated code

---
+++ AKTU Semester C program/Prog2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// function declaration
+void functionName();
+
+int main() {
+    functionName();
+    return 0;
+}
+
+// function definition
+void functionName() {
+    // logic here
+}
+
