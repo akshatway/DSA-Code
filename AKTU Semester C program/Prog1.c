19c9c46281bc872fdb522cd567229efc3541d161
Akshat <akshat@gmail.com>
2026-04-27 17:14:09 +0530

    new

--- AKTU Semester C program/Prog1.c
+++ AKTU Semester C program/Prog1.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
-
 // 1. WAP that accepts the marks of 5 subjects and finds the sum and percentage marks obtained
-void student_result();//prototype
-//main function
-int main() {
+void student_result(); // prototype
+// main function
+int main()
+{
     student_result();
     return 0;
 }
 
 // function definition
-void student_result() {
+void student_result()
+{
     // logic here
-    int m1,m2,m3,m4,m5,sum;
+    int m1, m2, m3, m4, m5, sum;
     float percentage;
     printf("Enter All five subject marks one by one:");
-    scanf("%d%d%d%d%d",&m1,&m2,&m3,&m4,&m5);
-    sum=m1+m2+m3+m4+m5;
-    percentage=sum/5.0;
-    printf("\n Total Marks is %d\n Your percentage is %.2f",sum,percentage);
+    scanf("%d%d%d%d%d", &m1, &m2, &m3, &m4, &m5);
+    sum = m1 + m2 + m3 + m4 + m5;
+    percentage = sum / 5.0;
+    printf("\n Total Marks is %d\n Your percentage is %.2f", sum, percentage);
     return;
 }
